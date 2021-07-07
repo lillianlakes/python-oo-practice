54e5c2dbd7cd02eaf565da3cefff1cad18119288
lancelee2885 <lancelee2885@gmail.com>
2021-07-06 17:32:52 -0700

    python oo finished

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,22 +20,22 @@ class SerialGenerator:
 
     def __init__(self, start):
         """Creates a serial generator from the number start"""
-        
+
         self.start = start
         self.counter = -1
-    
+
     def __repr__(self):
         """Describes this instance of SerialGenerator"""
-        return f'<SerialGenerator start = {self.start}>'
+
+        return f"<SerialGenerator start={self.start}, next={self.start + self.counter + 1}>"
 
     def generate(self):
         """Returns next sequential number of start"""
-        
+
         self.counter += 1
         return self.start + self.counter
-    
+
     def reset(self):
         """Resets start back to the original start number"""
-        
-        self.counter = -1
 
+        self.counter = -1
--- wordfinder.py
+++ wordfinder.py
@@ -1,12 +1,21 @@
 from random import choice
 
+
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a dictionary.
+
+    >>> wf = WordFinder("/usr/share/dict/words")
+    235886 words read
+
+    >>> type(wf.random())
+    <class 'str'>
+
+    """
 
     def __init__(self, file_path):
         """Reads a file containing different words on each line and makes an attribute of a list of those words,
         then prints out the number of words read"""
-
+        self.file_path = file_path
         self.file = open(file_path)
         self.words = []
         self.create_words()
@@ -25,8 +34,8 @@ class WordFinder:
     def print_message(self):
         """Prints a message with the number of words read"""
 
-        self.message = f"{len(self.words)} words read"   
-        print(self.message)   
+        self.message = f"{len(self.words)} words read"
+        print(self.message)
 
     def random(self):
         """Selects a random word for the list"""
@@ -35,10 +44,16 @@ class WordFinder:
 
 
 class SpecialWordFinder(WordFinder):
-    """Special Word Finder: finds random words from a dictionary, with blanks and lines starting with the # symbol removed."""
+    """Special Word Finder: finds random words from a dictionary, with blanks and lines starting with the # symbol removed.
+
+    >>> type(wf.random())
+    str
+
+    """
 
     def create_words(self):
         """Turns the file into a list of words without blanks and lines starting with the # symbol"""
 
         super().create_words()
-        self.words = [word for word in self.words if word and not word.startswith('#')]
+        self.words = [
+            word for word in self.words if word and not word.startswith('#')]

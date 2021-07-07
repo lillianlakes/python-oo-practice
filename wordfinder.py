ee59e1b3101c3f265445910ef5b5df3bc6ec70b0
lillianlakes <lillian.lakes@gmail.com>
2021-07-06 17:02:44 -0700

    adds SpecialWordFinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,26 +1,44 @@
 from random import choice
 
-
 class WordFinder:
-
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, file_path):
-        """Open up a file containing different words in each line and call create_words_list method"""
+        """Reads a file containing different words on each line and makes an attribute of a list of those words,
+        then prints out the number of words read"""
 
         self.file = open(file_path)
-        self.words_list = []
-        self.create_words_list()
+        self.words = []
+        self.create_words()
+        self.print_message()
 
-    def create_words_list(self):
-        """Turn the file into a list of words"""
+    def __repr__(self):
+        """Describes this class"""
 
-        for word in self.file:
-            word = word.strip()
-            self.words_list.append(word)
-        print(f"{len(self.words_list)} words read")
+        return f'<{self.__class__} file_path={self.file_path}>'
+
+    def create_words(self):
+        """Turns the file into a list of words"""
+
+        self.words = [word.strip() for word in self.file]
+
+    def print_message(self):
+        """Prints a message with the number of words read"""
+
+        self.message = f"{len(self.words)} words read"   
+        print(self.message)   
 
     def random(self):
-        """Select a random word for the list"""
-        return choice(self.words_list)
+        """Selects a random word for the list"""
 
+        return choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """Special Word Finder: finds random words from a dictionary, with blanks and lines starting with the # symbol removed."""
+
+    def create_words(self):
+        """Turns the file into a list of words without blanks and lines starting with the # symbol"""
+
+        super().create_words()
+        self.words = [word for word in self.words if word and not word.startswith('#')]
